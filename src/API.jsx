fdb233767083b3aa88bb77e1b075afa538332816
Oleksandr Levchuk <emihrant.ua@gmail.com>
2023-06-05 19:54:43 +0200

    Update API.jsx

--- src/API.jsx
+++ src/API.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://63c27790b0c286fbe5ee1d8c.mockapi.io/users/';
+axios.defaults.baseURL = 'https://63c27790b0c286fbe5ee1d8c.mockapi.io/';
 
 export const fetchUsers = async () => {
   const response = await axios.get('/Users');
