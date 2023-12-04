93d231214c9915e563dc9c3846a3770548b51aca
Oleksandr Levchuk <emihrant.ua@gmail.com>
2023-12-04 20:36:10 +0100

    initial app

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/test-task">
+    <BrowserRouter basename="/pet-project-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
